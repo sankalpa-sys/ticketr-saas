02bdc44e0d2898ace94c2be33e69a572e153052d
sankalpaneupane <sneupane@hamropatro.com>
2024-12-06 15:02:39 +0545

    fix: chnage title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ import SyncUserWithConvex from "@/components/SyncUserWithConvex";
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: "Ticketing SAAS",
-  description: "Your comprehensive ticketing platform",
+  title: "Ticketr: Anyone can sell tickets",
+  description:
+    "Ticketr is a revolutionary ticketing platform that allows anyone to become a ticket seller effortlessly. Whether you're hosting a small gathering, a large-scale event, or managing recurring activities, Ticketr gives you the tools to create, manage, and sell tickets with ease. Our user-friendly interface, secure payment options, and real-time analytics make it simple for individuals and businesses to connect with their audiences. With Ticketr, event management is no longer limited to professionals—anyone can now sell tickets and share experiences seamlessly.",
 };
 
 export default function RootLayout({
